4e31b5c0e38d98880cc1a691294fc5a20bcb1313
ccnmagnoo <crcamposn@gmail.com>
2023-11-24 11:30:46 -0300

    type helpers

---
+++ src/app/db/scope.ts
@@ -0,0 +1,13 @@
+// cspell:disable
+type Scope =
+  | 'nacional'
+  | 'regional'
+  | 'servicio de salud'
+  | 'comunal'
+  | 'provincial'
+  | 'circunscripción'
+  | 'distrital'
+  | 'territorio maritimo'
+  | 'prefectura';
+
+export default Scope;
