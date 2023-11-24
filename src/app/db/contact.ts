95b1ede9ecb763967b25c802a09175fcdfa79f28
ccnmagnoo <crcamposn@gmail.com>
2023-11-24 11:57:45 -0300

    restrict types values on scope territory and intitution

--- src/app/db/contact.ts
+++ src/app/db/contact.ts
@@ -1,12 +1,15 @@
+import Institution from './Institution';
+import Territory from './Territory';
 import contact from './contact.json';
+import Scope from './scope';
 
 //base Contact structure
 type ContactStruct = {
   name: string;
   position: string;
-  institution: string;
-  scope: string;
-  territory: string;
+  institution: Institution;
+  scope: Scope;
+  territory: Territory;
   email: string;
   phone: number;
   movil: number;
@@ -23,4 +26,4 @@ export type Nullable<TYPE> = {
 export type ContactJson = Nullable<ContactStruct>;
 
 //db with contacts
-const data: ContactJson[] = contact;
+const data = contact as ContactJson[];
