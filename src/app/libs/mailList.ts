e41d17ffbb591f9c71dece1bb0099c15448deb92
ccnmagnoo <crcamposn@gmail.com>
2023-11-24 15:51:01 -0300

    init class with contact Generator

---
+++ src/app/libs/mailList.ts
@@ -0,0 +1,17 @@
+import Institution from '../db/Institution';
+import Territory from '../db/Territory';
+import { ContactAssembler, ContactJson, contactData } from '../db/contact';
+import Scope from '../db/scope';
+
+class MailListGen<MODEL extends keyof ContactAssembler, KEY extends ContactJson[MODEL]> {
+  contacts = contactData;
+  private model: MODEL;
+  private assembler: KEY[];
+
+  constructor(model: MODEL, assembler: KEY[]) {
+    this.model = model;
+    this.assembler = assembler;
+  }
+}
+
+const list = new MailListGen('institution', ['Armada', 'Aduana']);
