4a00a9ca7986440aecd560e801f379dcd5f08c67
ccnmagnoo <crcamposn@gmail.com>
2023-11-24 12:54:45 -0300

    remove unnecessary adapter

--- src/app/libs/gobmailer.ts
+++ src/app/libs/gobmailer.ts
@@ -1,21 +1,14 @@
 render;
 import { render } from '@react-email/render';
-import { MailerElement, MailerOption, MailerStatus, transporter } from './transporter';
+import { MailerStatus, transporter } from './transporter';
 import Mail from 'nodemailer/lib/mailer';
 
 // cspell:disable
 export async function gobmailer(option: Mail.Options): Promise<MailerStatus> {
-  const compose: Mail.Options = {
-    from: option.from,
-    to: option.to,
-    cc: option.cc,
-    subject: option.subject,
-    html: option.html,
-  };
   try {
-    await transporter.sendMail(compose);
+    await transporter.sendMail(option);
     console.log('mail sent');
-    return { status: 'OK', details: `sent mail to ${compose.to}` };
+    return { status: 'OK', details: `sent mail to ${option.to}` };
   } catch (error) {
     console.error('mail error', error);
     return {
