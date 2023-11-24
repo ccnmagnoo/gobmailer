192c8aac45c2fe5022ee3d2a242db153e23ce699
ccnmagnoo <crcamposn@gmail.com>
2023-11-24 17:55:51 -0300

    quick testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
+import MailListGen from './libs/MailList';
 import styles from './page.module.scss';
 
 export default function Home() {
+  const list = new MailListGen('institution', ['Aduana', 'Armada']);
+
+  console.log(list.mailList);
+
   return (
     <main className={styles.main}>
       <header>
