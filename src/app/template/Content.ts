0664671a514fa3c3d5bc66525d0d185504ec2a35
ccnmagnoo <crcamposn@gmail.com>
2023-11-27 16:20:59 -0300

    dynamic email content type

---
+++ src/app/template/Content.ts
@@ -0,0 +1,25 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from '@react-email/components';
+
+export interface Content {
+  subject: string;
+  title: string;
+  salute?: string;
+  message: string;
+  location?: string;
+  address: string;
+  map?: (address: string) => string;
+  date: Date;
+}
