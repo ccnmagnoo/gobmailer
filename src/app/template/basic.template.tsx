99d32b5fbfe5698d3d5eefe5fb7ca609d664bba8
ccnmagnoo <crcamposn@gmail.com>
2023-11-27 16:21:15 -0300

    basic template

---
+++ src/app/template/basic.template.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Content } from './Content';
+import { Html } from '@react-email/html';
+
+export const Basic = (props: Content) => {
+  return <Html lang='es' dir='ltr'></Html>;
+};
--- src/app/template/test.template.tsx
+++ src/app/template/test.template.tsx
@@ -2,7 +2,7 @@ import { Button } from '@react-email/button';
 import { Html } from '@react-email/html';
 import React from 'react';
 
-export const TestMail = ({ name }: { name: string }) => {
+export const Mail = ({ name }: { name: string }) => {
   return (
     <Html>
       este es un test {name}
