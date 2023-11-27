8c4504dd9f2f8bd70f9add7e36965bec792e3edd
ccnmagnoo <crcamposn@gmail.com>
2023-11-27 09:45:26 -0300

    mailList

--- src/app/libs/mailList.ts
+++ src/app/libs/mailList.ts
@@ -3,7 +3,7 @@ import { ContactAssembler, ContactJson, contactData } from '../db/contact';
 class MailListGen<MODEL extends keyof ContactAssembler, KEY extends ContactJson[MODEL]> {
   private contacts = contactData;
   private model: MODEL; //institution,territory,scope
-  private assembler: KEY[]; // correspoding element of institution or territory or scope
+  private assembler: KEY[]; // corresponding element of institution or territory or scope
 
   constructor(model: MODEL, assembler: KEY[]) {
     this.model = model;
@@ -20,6 +20,10 @@ class MailListGen<MODEL extends keyof ContactAssembler, KEY extends ContactJson[
     });
 
     return rev;
+  }
+
+  groupByMails() {
+    this.mailList.forEach(() => {});
   }
 }
 
