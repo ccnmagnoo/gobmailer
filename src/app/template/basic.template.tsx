d93fcc5a797adc613709667e605258c6cd75d658
ccnmagnoo <crcamposn@gmail.com>
2023-11-28 16:09:44 -0300

    mail content

--- src/app/template/basic.template.tsx
+++ src/app/template/basic.template.tsx
@@ -1,7 +1,37 @@
 import React from 'react';
-import { Content } from './Content';
+import { Content, Review } from './Content';
 import { Html } from '@react-email/html';
+import { Body, Container, Head, Preview, Section } from '@react-email/components';
 
-export const Basic = (props: Content) => {
-  return <Html lang='es' dir='ltr'></Html>;
+interface Props extends Content {}
+
+export const Basic = (c: Props) => {
+  const previewText = c.title;
+  return (
+    <Html lang='es' dir='ltr'>
+      <Head></Head>
+      <Preview>{previewText}</Preview>
+      <Body style={main}>
+        <Container style={container}></Container>
+        <Section style={footer}></Section>
+      </Body>
+    </Html>
+  );
+};
+
+const main = {
+  backgroundColor: '#efeef1',
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  width: '580px',
+  margin: '30px auto',
+  backgroundColor: '#ffffff',
+};
+
+const footer = {
+  width: '580px',
+  margin: '0 auto',
 };
